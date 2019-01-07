274fa8a718c7ed5bb86baaac8291dd0d11a24b89
jjardine85 <jamesjardine@gmx.com>
2019-01-07 11:26:53 +0000

    guest count

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -6,7 +6,9 @@ require_relative("../guest")
 class RoomTest < MiniTest::Test
 
   def setup
+    @guest1 = Guest.new("Conor")
     @room = Room.new("one", 2, [])
+    @room1 = Room.new("two", 1, [@guest1])
   end
 
   def test_room_name
@@ -21,10 +23,16 @@ class RoomTest < MiniTest::Test
     assert_equal([], @room.guests)
   end
 
-  def test_can_count_guest
+  def test_can_count_guest__empty
     assert_equal(0, @room.guest_count)
   end
 
+  def test_can_count__not_empty
+    assert_equal(1, @room1.guest_count)
+  end
+  
+  def
 
+  end
 
 end
